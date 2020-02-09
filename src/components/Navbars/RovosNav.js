ed823d1cbd9306a5b43e8ea1d98cbf14bf96ce6a
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2020-02-09 10:59:13 +0200

    mobile nav fix

--- src/components/Navbars/RovosNav.js
+++ src/components/Navbars/RovosNav.js
@@ -75,7 +75,7 @@ function WhiteNavbar() {
                   </p>
                 </DropdownToggle>
                 <DropdownMenu aria-labelledby="navbarDropdownMenuLink1" right>
-                  <DropdownItem to="/details" tag={Link}>
+                  <DropdownItem to="/details" tag={Link} onClick={setCollapseOpen(false)}>
                     <i className="now-ui-icons design_bullet-list-67"></i>
                     Train Details
                   </DropdownItem>
